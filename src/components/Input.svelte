10f17d6acf616b6d66f85b36f92ed8ccaf9688a5
tvmche <tri.van.minh.che@mgm-tp.com>
2020-08-01 11:12:20 +0700

    Tasks store

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -1,19 +1,41 @@
 <script>
+	import { setObj } from "../helper/localStorage";
+	import { getObj } from "../helper/localStorage";
+	import { tasks_store } from "../store/store";
+
 	let task = "";
 
 	function handleKeypress(event) {
 		if (event.key === "Enter") {
-			// TODO Handle Enter
+			task = task.trim();
+			const currentTasks = getObj("tasks") || [];
+			const newTasks = [
+				...currentTasks,
+				task
+			]
+			setObj("tasks", newTasks);
+			tasks_store.update(() => newTasks);
+			task = "";
 		}
+	}
+
+	function handleClear() {
+		setObj("tasks", []);
+		tasks_store.update(() => []);
 	}
 </script>
 
 <input
 	type="text"
 	placeholder="Start task"
-	bind:value="{task}"
+	bind:value={task}
 	on:keypress={handleKeypress}
 />
+<button
+	on:click={handleClear}
+>
+	Clear
+</button>
 
 <style>
 	input {
--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -1,15 +1,40 @@
 <script>
 	import Input from "./Input.svelte";
 	import Container from "./Container.svelte";
+	import { onMount, onDestroy } from "svelte";
+	import { getObj } from "../helper/localStorage";
+	import { tasks_store } from "../store/store";
+
+	let tasks;
+	const unsubscribe = tasks_store.subscribe(newValue => {
+		tasks = newValue;
+	});
+
+	// TODO Load initial tasks
+	onMount(() => {
+		tasks = getObj("tasks");
+	});
+	onDestroy(() => {
+		unsubscribe();
+	});
 </script>
 
 <Container>
-	<h2>Hello World</h2>
+	<h2>Task Logger</h2>
 	<Input/>
+	<hr/>
+
+	{#if tasks && tasks.length}
+		{#each tasks as task}
+			<div>{task}</div>
+		{/each}
+		{:else}
+		<div>There is no available task!</div>
+	{/if}
 </Container>
 
 <style>
 	h2 {
-		color: red;
+		color: #ff0000;
 	}
 </style>
